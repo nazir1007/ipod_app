5a42c684dfe307b7744e71ba1ee003e9fe5ba3ce
Mohd Nazir <mohdnazir1007@gmail.com>
2022-08-15 10:26:58 +0530

    first commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,23 @@
+import wallpaper1 from './wallpaper1.jpg';
+import wallpaper2 from './wallpaper2.jpg';
+import wallpaper3 from './wallpaper3.jpg';
+import games from './games.jpg';
+import artists from './artists.jpg';
+import coverflow from './coverflow.png';
+import song1Img from './Unstoppable.jpg';
+import song2Img from './Somebody like you.jpg';
+import song3Img from './Let me down slowly.jpg';
+
+const images = {
+  wallpaper1,
+  wallpaper2,
+  wallpaper3,
+  games,
+  artists,
+  coverflow,
+  song1Img,
+  song2Img,
+  song3Img,
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from './Unstoppable.mp3';
+import music2 from './Somebody Like U.mp3';
+import music3 from './Let Me Down Slowly.mp3';
+
+const songs = {
+  music1,
+  music2,
+  music3,
+};
+
+export default songs;
